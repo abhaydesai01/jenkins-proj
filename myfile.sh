d945f5ea3ade2680779f635bd33150c6feaedc53
abhaydesai01 <70378731+abhaydesai01@users.noreply.github.com>
2021-06-16 17:45:10 +0530

    Update myfile.sh

--- myfile.sh
+++ myfile.sh
@@ -1,17 +1,31 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-keyname=Abhay2
-instancename=tryonce
-ebsname=tryebs
+#Declaring Variables
+keyname=testingclikey
+instancename=testinginstance
+sgname=testingsg
+ebsname=testingebs
+
+#creating the key pair
+aws ec2 create-key-pair --key-name $keyname --tag-specification "ResourceType=key-pair,Tags=[{Key=Name,Value=$keyname}]" --query "KeyMaterial" --output text > $keyname.pem
+
+#Creating the security group
+aws ec2 create-security-group --group-name $sgname --description "created from CLI"  --vpc-id vpc-50f0e456
+
+aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --filters Name=group-name,Values=$sgname
+
+sgid=`aws ec2 describe-security-groups  --query "SecurityGroups[].GroupId" --filters "Name=group-name,Values=$sgname" | sed -n 2p | tr -d \"`
 
 
+#Adding the inbound rules to the security group created
+aws ec2 authorize-security-group-ingress --group-id $sgid --protocol tcp --port 22 --cidr 0.0.0.0/0
 
-aws ec2 run-instances --image-id ami-0ad704c126371a549 --count 1 --instance-type t2.micro --key-name $keyname --security-group-ids sg-04fa0c7c --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$instancename}]"
+
+aws ec2 run-instances --image-id ami-0ad704c126371a549 --count 1 --instance-type t2.micro --key-name Abhay2 --security-group-ids  sg-04fa0c7c  --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$instancename}]"
 
 
 #Creating the Volume
 aws ec2 create-volume --availability-zone ap-south-1a --size 5 --tag-specification "ResourceType=volume,Tags=[{Key=Name,Value=$ebsname}]"
-
 
 #storing the value of Volume ID in variable volumeID
 volumeID=`aws ec2 describe-volumes  --query "Volumes[*].VolumeId" --filters "Name=tag:Name,Values=$ebsname" | sed -n 2p | tr -d \"`
